ce26da53e111f808da5620eae970306d34988746
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2024-10-01 18:10:06 -0600

    refact: view:

--- src/views/LoginUser.vue
+++ src/views/LoginUser.vue
@@ -1,10 +1,19 @@
 <script lang="ts" setup>
 import LoginUserLayout from '@/layouts/LoginUserLayout.vue'
+import FormLogin from '@/components/formLogin/FormLogin.vue'
+import ContentFormUser from '@/components/contentFormUser/ContentFormUser.vue'
 </script>
 
 <template>
   <LoginUserLayout>
-    <h1>bbbbbb</h1>
+    <ContentFormUser>
+      <template v-slot:menu>
+        <h1>menu</h1>
+      </template>
+      <template v-slot:form>
+        <FormLogin />
+      </template>
+    </ContentFormUser>
   </LoginUserLayout>
 </template>
 
