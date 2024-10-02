fa3898ef2ba9b64239b7d2120be452761bd066c3
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2024-10-01 18:26:50 -0600

    refact: name componest and files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginUser from '../views/LoginUser.vue'
+import SessionView from '../views/SessionView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
-      name: 'login',
-      component: LoginUser
+      path: '/session',
+      name: 'session',
+      component: SessionView
     },
     {
       path: '/',
---
+++ src/views/SessionView.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import SessionLayout from '@/layouts/SessionLayout.vue'
+import SessionContainer from '@/components/sessionContainer/SessionContainer.vue'
+import LoginForm from '@/components/loginForm/LoginForm.vue'
+</script>
+
+<template>
+  <SessionLayout>
+    <SessionContainer>
+      <template v-slot:menu>
+        <h1>menu</h1>
+      </template>
+      <template v-slot:form>
+        <LoginForm />
+      </template>
+    </SessionContainer>
+  </SessionLayout>
+</template>
+
+<style lang="postcss" scoped></style>
