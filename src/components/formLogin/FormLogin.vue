a1f82e5e9afb0424522a921df3457a2ad024080b
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2024-10-01 18:03:36 -0600

    feat: component : FormLogin

---
+++ src/components/formLogin/FormLogin.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div :class="$style.container">
+    <form :class="$style.container_form">
+      <h2>Login</h2>
+    </form>
+  </div>
+</template>
+
+<style lang="postcss" module>
+.container {
+  display: flex;
+  width: 90%;
+  height: 100%;
+  background: #0f172a80;
+  box-shadow: 0px 0px 0px 0.5px #ffffff97;
+  border-radius: 12px;
+}
+
+.container_form {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 20px;
+  border-radius: inherit;
+}
+</style>
