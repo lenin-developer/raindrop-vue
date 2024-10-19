700a2d9521c73e4fb65a45e29a3be50cee7c0861
Lenin cm <cruzleni@globalhitss.com>
2024-10-18 19:27:36 -0600

    feat: add layout

--- src/components/sessionContainer/SessionContainer.vue
+++ src/components/sessionContainer/SessionContainer.vue
@@ -2,12 +2,16 @@
 
 <template>
   <section :class="$style.wrapper">
-    <div :class="$style.children">
+    <div :class="$style.wrapper__layout">
+      <slot name="menu"></slot>
+      <slot name="form"></slot>
+    </div>
+    <!-- <div :class="$style.children">
       <slot name="menu"></slot>
     </div>
     <div :class="$style.children">
       <slot name="form"></slot>
-    </div>
+    </div> -->
   </section>
 </template>
 
@@ -22,6 +26,15 @@
   backdrop-filter: blur(8px);
   border-radius: 12px;
   box-shadow: 0px 0px 0px 3px var(--color-green-100);
+}
+
+.wrapper__layout {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 60px;
+  margin-bottom: 60px;
+  gap: 60PX;
 }
 
 .children {
