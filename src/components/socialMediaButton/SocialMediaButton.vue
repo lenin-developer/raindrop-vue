63c8ef8ea685739036b1f83f7ffd2f209f022812
Lenin cm <cruzleni@globalhitss.com>
2024-11-26 12:14:03 -0600

    new componenent social btn

---
+++ src/components/socialMediaButton/SocialMediaButton.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps<{
+    text?: string
+}>()
+
+</script>
+
+
+<template>
+  <button type="button" :class="$style.btn" >
+    <span :class="$style.btn__span" >
+        <slot></slot>
+    </span>
+    {{ text }}
+  </button>
+</template>
+
+<style lang="postcss" module>
+.btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 48px;
+    width: 100%;
+    border: none;
+    border-radius: 6px;
+    background-color: var(--color-gray-400);
+    cursor: pointer;
+}
+
+.btn:active {
+    transform: scale(0.995);
+}
+
+.btn__span {
+    display: flex;
+    align-items: center;
+    margin-right: 15px;
+}
+
+
+
+</style>
