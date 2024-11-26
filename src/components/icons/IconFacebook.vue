30004151df5206f690f2bcb7e9b9d44ecca6a784
Lenin cm <cruzleni@globalhitss.com>
2024-11-26 12:11:27 -0600

    add icons y create componente icons

---
+++ src/components/icons/IconFacebook.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+type Props = {
+    size?: number
+    height?: number
+    width?: number
+}
+
+
+const props: Props = defineProps<Props>();
+
+const height = computed(() => {
+    let height = 32
+    if ('height' in props && props?.height) {
+        height = props?.height
+    }
+    if ('size' in props && props?.size) {
+        height = props?.size
+    }
+    return height
+});
+
+const width = computed(() => {
+    let width = 32
+    if ('width' in props && props?.width) {
+        width = props?.width
+    }
+    if ('size' in props && props?.size) {
+        width = props?.size
+    }
+    return width
+});
+</script>
+
+<template>
+    <svg :width="width.toString()" :height="height.toString()" viewBox="0 0 16 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M13.0789 5.31333H16V0.225333C15.496 0.156 13.7629 0 11.7444 0C2.50246 0 5.01692 10.4667 4.64895 12H0V17.688H4.64761V32H10.3458V17.6893H14.8054L15.5134 12.0013H10.3445C10.5951 8.236 9.32989 5.31333 13.0789 5.31333V5.31333Z"
+            fill="#3B5999" />
+    </svg>
+
+</template>
---
+++ src/components/icons/IconMail.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+type Props = {
+    size?: number
+    height?: number
+    width?: number
+}
+
+
+const props: Props = withDefaults(defineProps<Props>(), {
+})
+
+
+
+const height = computed(() => {
+    let height = 32
+    if ('height' in props && props?.height) {
+        height = props?.height
+    }
+    if ('size' in props && props?.size) {
+        height = props?.size
+    }
+    return height
+});
+
+const width = computed(() => {
+    let width = 32
+    if ('width' in props && props?.width) {
+        width = props?.width
+    }
+    if ('size' in props && props?.size) {
+        width = props?.size
+    }
+    return width
+});
+</script>
+
+
+<template>
+    <svg :width="width?.toString()" :height="height?.toString()" viewBox="0 0 30 24" fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path 
+            d="M27 0H3C1.35 0 0.015 1.35 0.015 3L0 21C0 22.65 1.35 24 3 24H27C28.65 24 30 22.65 30 21V3C30 1.35 28.65 0 27 0ZM27 6L15 13.5L3 6V3L15 10.5L27 3V6Z"
+            fill="none" />
+    </svg>
+</template>
+
+
+<style lang="postcss" scoped>
+svg, path {
+    fill: black;
+}
+
+</style>
