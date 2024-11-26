22b549373bf6b1fecb1c3f22e11486712e81f090
Lenin cm <cruzleni@globalhitss.com>
2024-11-26 12:13:35 -0600

    refact: login Fomr

--- src/components/loginForm/LoginForm.vue
+++ src/components/loginForm/LoginForm.vue
@@ -1,11 +1,56 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import SocialMediaButton from '@/components/socialMediaButton/SocialMediaButton.vue'
+import HrCustom from '@/components/hrCustom/HrCustom.vue'
+import InputBase from '@/components/inputBase/InputBase.vue'
+import WrapperInputIcons from '@/components/wrapperInputIcons/WrapperInputIcons.vue'
+import ButtonBase from '@/components/buttonBase/ButtonBase.vue'
+
+import IconGoogle from '@/components/icons/IconGoogle.vue'
+import IconFacebook from '@/components/icons/IconFacebook.vue'
+import IconMail from '@/components/icons/IconMail.vue'
+import IconKey from '@/components/icons/IconKey.vue'
+
+</script>
 
 <template>
-  <div :class="$style.container">
-    <form :class="$style.container_form">
-      <h2>Login</h2>
-    </form>
-  </div>
+  <!-- <div :class="$style.container"> -->
+  <form :class="$style.container_form">
+    <SocialMediaButton text="Login wiht Google">
+      <IconGoogle />
+    </SocialMediaButton>
+    <SocialMediaButton text="Login wiht Facebook">
+      <IconFacebook />
+    </SocialMediaButton>
+
+    <HrCustom text="OR" />
+
+    <InputBase label="Email" id="correo" :class="$style.InputBase" autocomplete="off">
+      <template #icon>
+        <IconMail :class="$style.colorIcon" />
+      </template>
+    </InputBase>
+
+
+
+    <InputBase type="password" label="Password" id="password" :class="$style.InputBase" autocomplete="off">
+      <template #icon>
+        <IconKey :class="$style.colorIcon" />
+      </template>
+    </InputBase>
+
+
+
+    <InputBase type="password" label="Password repit" id="password" :class="$style.InputBase" autocomplete="off">
+      <template #icon>
+        <IconKey :class="$style.colorIcon" />
+      </template>
+    </InputBase>
+
+
+    <ButtonBase> hola </ButtonBase>
+
+  </form>
+  <!-- </div> -->
 </template>
 
 <style lang="postcss" module>
@@ -19,10 +64,36 @@
 }
 
 .container_form {
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  width: 90%;
   height: 100%;
   margin: 0;
   padding: 20px;
   border-radius: inherit;
+  gap: 30px;
+}
+
+/* modificadores */
+
+.InputBase {
+  font-weight: 800;
+}
+
+.colorIcon path {
+  fill: var(--color-green-100);
+}
+
+.colorIcon {
+  zoom: 0.6;
+}
+
+.wrapperInputIcons {
+  align-items: flex-end;
+
+}
+
+.wrapperInputIcons>div {
+  padding-left: 0px;
 }
 </style>
