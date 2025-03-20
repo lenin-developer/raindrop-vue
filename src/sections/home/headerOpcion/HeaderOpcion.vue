25e1edb6f1f03e5bd371ce74fb2676ddee1fea4e
Lenin cm <cruzleni@globalhitss.com>
2025-03-20 10:15:27 -0600

    fetac: init header

---
+++ src/sections/home/headerOpcion/HeaderOpcion.vue
@@ -0,0 +1,29 @@
+<script setup lang='ts'>
+import HaderOpcionLayout from './layout/HeaderOpcionLayout.vue';
+import { Icon } from "@iconify/vue";
+
+defineProps<{
+    isPanelOpen: boolean
+}>();
+
+
+
+</script>
+
+<template>
+    <HaderOpcionLayout>
+        <template #sectionTop>
+            <button v-if="!isPanelOpen" @click="$emit('showPanel')" class="util_btn_content_icon " >
+                <Icon icon="majesticons:menu-expand-left" width="100%" height="100%"
+                     />
+            </button>
+            <h3>top-header</h3>
+        </template>
+        <template #sectionBottom>
+            <h3>bottom-header</h3>
+        </template>
+    </HaderOpcionLayout>
+</template>
+
+<style lang='postcss' module>
+</style>
---
+++ src/sections/home/headerOpcion/layout/HeaderOpcionLayout.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+
+
+</script>
+
+<template>
+    <header :class="$style.layout" >
+        <section>
+            <slot name="sectionTop"></slot>
+        </section>
+        <section>
+            <slot name="sectionBottom"></slot>
+        </section>
+    </header>
+</template>
+
+<style lang='postcss' module>
+.layout {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+}
+</style>
