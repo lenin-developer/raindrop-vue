d88b9046308f5143aa39604fc87828a9a632e1fd
Lenin cm <cruzleni@globalhitss.com>
2025-03-27 12:55:51 -0600

    feat: new component input

---
+++ src/components/inputLabel/InputLabel.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { ref, type InputHTMLAttributes, type Ref } from 'vue'
+import InputBase from '@/components/inputBase/InputBase.vue';
+
+
+defineOptions({ inheritAttrs: false });
+
+type Props = {
+    label?: string,
+    id?: string,
+    classes?: string
+} & /* @vue-ignore */ Omit<InputHTMLAttributes, 'id'>;
+
+defineProps<Props>()
+
+const inputValue = ref('') as Ref<string> ;
+
+</script>
+
+<template>
+    <div :class="[$style.inputBase, classes]">
+        <div :class="$style.inputBase__sectionInf" >
+            <div :for="id?.toString()" v-if="$slots.icon"   >
+                <slot name="icon"></slot>
+            </div>
+            <label v-if="label" :for="id?.toString()" :class="$style.inputBase_label" :aria-label="label" >{{ label }}</label>
+        </div>
+         <InputBase v-model="inputValue" :id="id?.toString()" :class="$style.inputBase_input"  v-bind="$attrs"/>
+    </div>
+</template>
+
+<style lang="postcss" module>
+.inputBase {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    width: 100%;
+}
+
+.inputBase__sectionInf {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+}
+
+.inputBase__sectionInf > div {
+    height: 1rem;
+    display: flex;
+    align-items: center;
+}
+
+.inputBase_label {
+    font-weight: 200;
+}
+
+.inputBase_input {
+    padding: 8px;
+    outline: 2px solid var(--color-gray-300);
+    border-radius: 6px;
+}
+
+.inputBase_input:focus {
+    outline: 2px solid var(--color-green-100);
+}
+</style>
