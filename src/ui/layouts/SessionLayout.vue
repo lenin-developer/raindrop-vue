acb63c0e4b29a23b4fc9ed57fa1d0af8c20c47ab
Lenin cm <cruzleni@globalhitss.com>
2025-04-01 13:25:37 -0600

    refactor: layout responsibility

--- src/ui/layouts/SessionLayout.vue
+++ src/ui/layouts/SessionLayout.vue
@@ -7,7 +7,12 @@
     </video>
     <div :class="$style.loginUserLayout__overly"></div>
     <div :class="$style.loginUserLayout__containerSlot">
-      <slot></slot>
+      <section :class="$style.sessionContainer">
+        <div :class="$style.sessionContainer__layout">
+          <slot name="menu"></slot>
+          <slot name="form"></slot>
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -47,5 +52,36 @@
   opacity: var(--opacity-05);
   /* mix-blend-mode: overlay;  */
   height: 100%;
+}
+
+.sessionContainer {
+  position: absolute;
+  right: 10%;
+  top: 3%;
+  width: 500px;
+  background-color: #020420ae;
+  backdrop-filter: blur(8px);
+  border-radius: 12px;
+  box-shadow: 0px 0px 0px 2px var(--color-secondary);
+
+  @media(--md-viewport) {
+    width: calc(100% - 20%);
+  }
+
+  @media(--sm-viewport){
+    width: calc(100% - 6%);
+    right: 3%;
+  }
+
+}
+
+.sessionContainer__layout {
+  height: 650px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding-top: 60px;
+  gap: 60PX;
 }
 </style>
--- src/ui/views/session/SessionView.vue
+++ src/ui/views/session/SessionView.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { ref, type Ref } from 'vue';
 import {SessionLayout} from '@/ui/layouts'
-import { LoginForm, SessionForm, SwichMenu } from '@/ui/components';
+import { LoginForm, SwichMenu } from '@/ui/components';
 import type { SwitchItem } from '@/ui/components/switchMenu/SwitchItem';
 
 
@@ -18,21 +18,19 @@ const items: SwitchItem[] = [
 
 const loginOrSignIn = ref({ ...items?.[0] }) as Ref<SwitchItem>;
 
-
-
 </script>
+
 
 <template>
   <SessionLayout>
-    <SessionForm>
       <template v-slot:menu>
         <SwichMenu :items="items" v-model="loginOrSignIn" />
       </template>
       <template v-slot:form>
         <LoginForm :session="loginOrSignIn" />
       </template>
-    </SessionForm>
   </SessionLayout>
 </template>
+
 
 <style lang="postcss" scoped></style>
