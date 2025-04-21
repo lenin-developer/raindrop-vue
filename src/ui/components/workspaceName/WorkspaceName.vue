04438228f519d32797f7c154c559c9f874907221
Lenin cm <cruzleni@globalhitss.com>
2025-04-21 12:18:37 -0600

    feat:  avance panel workspace

---
+++ src/ui/components/workspaceName/WorkspaceName.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+
+defineProps<{
+    groupName: string,
+    name: string,
+    isDefaul: boolean
+}>();
+
+</script>
+
+<template>
+    <ul>
+        <li>
+            <input type="radio" :name="groupName" value="name" >
+            <p>{{ name }}</p>
+        </li>
+    </ul>
+
+</template>
+
+<style lang='postcss' module></style>
